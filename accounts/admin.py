6247f7801ed9bf4f39832db0888ffccb80e40d9f
User <user@Users-MacBook-Air.local>
2022-09-13 23:13:56 +0530

    Poastgres, user model functionality added

---
+++ accounts/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from .models import User
+
+# Register your models here.
+
+# To make passwords noneditable in admin panel
+class CustomUserAdmin(UserAdmin):
+    list_display = ('email',  'first_name', 'last_name','username', 'role', 'is_active')
+    ordering = ('-date_joined',)
+    # to disable password
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets=()
+
+admin.site.register(User, CustomUserAdmin)
--- foodOnline_main/settings.py
+++ foodOnline_main/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-
+from decouple import config
+ 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-40wfs@ubv@$jzyca9ya86qo_b4ns533_0u)&*&tmn#*8vpbq(='
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = []
 
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
@@ -75,11 +77,15 @@ WSGI_APPLICATION = 'foodOnline_main.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST')
     }
 }
 
+AUTH_USER_MODEL = 'accounts.User'     #tells django that we are using our custom user model, accounts app -> User class
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
